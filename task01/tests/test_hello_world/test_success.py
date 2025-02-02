f43c4c279a48691e139547a38a48e7ca5e9f8210
udanilau <udanilau@PMINTL.NET>
2025-02-02 18:34:51 +0400

    init commit

---
+++ task01/tests/test_hello_world/test_success.py
@@ -0,0 +1,13 @@
+from tests.test_hello_world import HelloWorldLambdaTestCase
+
+
+class TestSuccess(HelloWorldLambdaTestCase):
+
+    def test_success(self):
+        expected_response = {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+        }
+        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), expected_response)
+
+
